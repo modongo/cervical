0e9e455941697c5edb1fb5f98b5cac401a9d74e7
Michael Odongo <mikodongo@gmail.com>
2019-09-27 03:31:45 +0300

    first commit

---
+++ src/main/java/ke/cerbre/mscervicalcancer/MsCervicalBreastCancerApplication.java
@@ -0,0 +1,13 @@
+package ke.cerbre.mscervicalcancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCervicalBreastCancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCervicalBreastCancerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ke/cerbre/mscervicalcancer/controller/PayPalController.java
@@ -0,0 +1,31 @@
+package ke.cerbre.mscervicalcancer.controller;
+
+import ke.cerbre.mscervicalcancer.services.PayPalClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "https://cervical.herokuapp.com/")
+@RequestMapping(value = "/paypal")
+public class PayPalController {
+
+    private final PayPalClient payPalClient;
+
+    @Autowired
+    PayPalController(PayPalClient payPalClient){
+        this.payPalClient = payPalClient;
+    }
+
+    @PostMapping(value = "/make/payment")
+    public Map<String, Object> makePayment(@RequestParam("sum") String sum){
+        return payPalClient.createPayment(sum);
+    }
+
+    @PostMapping(value = "/complete/payment")
+    public Map<String, Object> completePayment(HttpServletRequest request){
+        return payPalClient.completePayment(request);
+    }
+}
